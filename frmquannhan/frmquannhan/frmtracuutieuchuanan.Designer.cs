aecbf66f01be19906a96ef2abf1726162b366191
xgiao <chipchipsieu@gmail.com>
2026-03-12 13:58:11 +0700

    Add files via upload

---
+++ frmquannhan/frmquannhan/frmtracuutieuchuanan.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace frmquannhan
+{
+    partial class frmtracuutieuchuanan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltieude = new System.Windows.Forms.Label();
+            this.lblhoten = new System.Windows.Forms.Label();
+            this.lblnhapten = new System.Windows.Forms.Label();
+            this.lbldonvi = new System.Windows.Forms.Label();
+            this.lblnhapdonvi = new System.Windows.Forms.Label();
+            this.lblchedo = new System.Windows.Forms.Label();
+            this.lblnhapchedo = new System.Windows.Forms.Label();
+            this.lbltienan = new System.Windows.Forms.Label();
+            this.lblnhaptieuchuan = new System.Windows.Forms.Label();
+            this.lblmaquannhan = new System.Windows.Forms.Label();
+            this.lblnhapma = new System.Windows.Forms.Label();
+            this.lbltongnl = new System.Windows.Forms.Label();
+            this.lblnhapnangluong = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colSTT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colthucpham = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coldinhluong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coldonvi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colkcal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbltieude
+            // 
+            this.lbltieude.AutoSize = true;
+            this.lbltieude.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lbltieude.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltieude.Location = new System.Drawing.Point(191, 22);
+            this.lbltieude.Name = "lbltieude";
+            this.lbltieude.Size = new System.Drawing.Size(342, 39);
+            this.lbltieude.TabIndex = 0;
+            this.lbltieude.Text = "Tiêu chuẩn ăn của bạn";
+            this.lbltieude.Click += new System.EventHandler(this.lbltieude_Click);
+            // 
+            // lblhoten
+            // 
+            this.lblhoten.AutoSize = true;
+            this.lblhoten.Location = new System.Drawing.Point(107, 79);
+            this.lblhoten.Name = "lblhoten";
+            this.lblhoten.Size = new System.Drawing.Size(109, 18);
+            this.lblhoten.TabIndex = 1;
+            this.lblhoten.Text = "Tên quân nhân";
+            // 
+            // lblnhapten
+            // 
+            this.lblnhapten.AutoSize = true;
+            this.lblnhapten.Location = new System.Drawing.Point(255, 79);
+            this.lblnhapten.Name = "lblnhapten";
+            this.lblnhapten.Size = new System.Drawing.Size(50, 18);
+            this.lblnhapten.TabIndex = 2;
+            this.lblnhapten.Text = "label3";
+            // 
+            // lbldonvi
+            // 
+            this.lbldonvi.AutoSize = true;
+            this.lbldonvi.Location = new System.Drawing.Point(110, 124);
+            this.lbldonvi.Name = "lbldonvi";
+            this.lbldonvi.Size = new System.Drawing.Size(54, 18);
+            this.lbldonvi.TabIndex = 3;
+            this.lbldonvi.Text = "Đơn vị";
+            // 
+            // lblnhapdonvi
+            // 
+            this.lblnhapdonvi.AutoSize = true;
+            this.lblnhapdonvi.Location = new System.Drawing.Point(255, 124);
+            this.lblnhapdonvi.Name = "lblnhapdonvi";
+            this.lblnhapdonvi.Size = new System.Drawing.Size(50, 18);
+            this.lblnhapdonvi.TabIndex = 4;
+            this.lblnhapdonvi.Text = "label5";
+            // 
+            // lblchedo
+            // 
+            this.lblchedo.AutoSize = true;
+            this.lblchedo.Location = new System.Drawing.Point(406, 124);
+            this.lblchedo.Name = "lblchedo";
+            this.lblchedo.Size = new System.Drawing.Size(80, 18);
+            this.lblchedo.TabIndex = 5;
+            this.lblchedo.Text = "Chế độ ăn";
+            // 
+            // lblnhapchedo
+            // 
+            this.lblnhapchedo.AutoSize = true;
+            this.lblnhapchedo.Location = new System.Drawing.Point(569, 124);
+            this.lblnhapchedo.Name = "lblnhapchedo";
+            this.lblnhapchedo.Size = new System.Drawing.Size(50, 18);
+            this.lblnhapchedo.TabIndex = 6;
+            this.lblnhapchedo.Text = "label7";
+            // 
+            // lbltienan
+            // 
+            this.lbltienan.AutoSize = true;
+            this.lbltienan.Location = new System.Drawing.Point(107, 177);
+            this.lbltienan.Name = "lbltienan";
+            this.lbltienan.Size = new System.Drawing.Size(132, 18);
+            this.lbltienan.TabIndex = 7;
+            this.lbltienan.Text = "Tiêu chuẩn tiền ăn";
+            this.lbltienan.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblnhaptieuchuan
+            // 
+            this.lblnhaptieuchuan.AutoSize = true;
+            this.lblnhaptieuchuan.Location = new System.Drawing.Point(255, 177);
+            this.lblnhaptieuchuan.Name = "lblnhaptieuchuan";
+            this.lblnhaptieuchuan.Size = new System.Drawing.Size(50, 18);
+            this.lblnhaptieuchuan.TabIndex = 8;
+            this.lblnhaptieuchuan.Text = "label1";
+            // 
+            // lblmaquannhan
+            // 
+            this.lblmaquannhan.AutoSize = true;
+            this.lblmaquannhan.Location = new System.Drawing.Point(406, 79);
+            this.lblmaquannhan.Name = "lblmaquannhan";
+            this.lblmaquannhan.Size = new System.Drawing.Size(105, 18);
+            this.lblmaquannhan.TabIndex = 9;
+            this.lblmaquannhan.Text = "Mã quân nhân";
+            // 
+            // lblnhapma
+            // 
+            this.lblnhapma.AutoSize = true;
+            this.lblnhapma.Location = new System.Drawing.Point(569, 79);
+            this.lblnhapma.Name = "lblnhapma";
+            this.lblnhapma.Size = new System.Drawing.Size(50, 18);
+            this.lblnhapma.TabIndex = 10;
+            this.lblnhapma.Text = "label2";
+            // 
+            // lbltongnl
+            // 
+            this.lbltongnl.AutoSize = true;
+            this.lbltongnl.Location = new System.Drawing.Point(406, 177);
+            this.lbltongnl.Name = "lbltongnl";
+            this.lbltongnl.Size = new System.Drawing.Size(127, 18);
+            this.lbltongnl.TabIndex = 11;
+            this.lbltongnl.Text = "Tổng năng lượng";
+            // 
+            // lblnhapnangluong
+            // 
+            this.lblnhapnangluong.AutoSize = true;
+            this.lblnhapnangluong.Location = new System.Drawing.Point(569, 177);
+            this.lblnhapnangluong.Name = "lblnhapnangluong";
+            this.lblnhapnangluong.Size = new System.Drawing.Size(50, 18);
+            this.lblnhapnangluong.TabIndex = 12;
+            this.lblnhapnangluong.Text = "label6";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSTT,
+            this.colthucpham,
+            this.coldinhluong,
+            this.coldonvi,
+            this.colkcal});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 241);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 236);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // colSTT
+            // 
+            this.colSTT.HeaderText = "STT";
+            this.colSTT.MinimumWidth = 8;
+            this.colSTT.Name = "colSTT";
+            this.colSTT.Width = 150;
+            // 
+            // colthucpham
+            // 
+            this.colthucpham.HeaderText = "Thực phẩm";
+            this.colthucpham.MinimumWidth = 8;
+            this.colthucpham.Name = "colthucpham";
+            this.colthucpham.Width = 150;
+            // 
+            // coldinhluong
+            // 
+            this.coldinhluong.HeaderText = "Định lượng";
+            this.coldinhluong.MinimumWidth = 8;
+            this.coldinhluong.Name = "coldinhluong";
+            this.coldinhluong.Width = 150;
+            // 
+            // coldonvi
+            // 
+            this.coldonvi.HeaderText = "Đơn vị";
+            this.coldonvi.MinimumWidth = 8;
+            this.coldonvi.Name = "coldonvi";
+            this.coldonvi.Width = 150;
+            // 
+            // colkcal
+            // 
+            this.colkcal.HeaderText = "kcal";
+            this.colkcal.MinimumWidth = 8;
+            this.colkcal.Name = "colkcal";
+            this.colkcal.Width = 150;
+            // 
+            // frmtracuutieuchuanan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(857, 489);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblnhapnangluong);
+            this.Controls.Add(this.lbltongnl);
+            this.Controls.Add(this.lblnhapma);
+            this.Controls.Add(this.lblmaquannhan);
+            this.Controls.Add(this.lblnhaptieuchuan);
+            this.Controls.Add(this.lbltienan);
+            this.Controls.Add(this.lblnhapchedo);
+            this.Controls.Add(this.lblchedo);
+            this.Controls.Add(this.lblnhapdonvi);
+            this.Controls.Add(this.lbldonvi);
+            this.Controls.Add(this.lblnhapten);
+            this.Controls.Add(this.lblhoten);
+            this.Controls.Add(this.lbltieude);
+            this.Name = "frmtracuutieuchuanan";
+            this.Text = "Tra cứu tiêu chuẩn ăn";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltieude;
+        private System.Windows.Forms.Label lblhoten;
+        private System.Windows.Forms.Label lblnhapten;
+        private System.Windows.Forms.Label lbldonvi;
+        private System.Windows.Forms.Label lblnhapdonvi;
+        private System.Windows.Forms.Label lblchedo;
+        private System.Windows.Forms.Label lblnhapchedo;
+        private System.Windows.Forms.Label lbltienan;
+        private System.Windows.Forms.Label lblnhaptieuchuan;
+        private System.Windows.Forms.Label lblmaquannhan;
+        private System.Windows.Forms.Label lblnhapma;
+        private System.Windows.Forms.Label lbltongnl;
+        private System.Windows.Forms.Label lblnhapnangluong;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSTT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colthucpham;
+        private System.Windows.Forms.DataGridViewTextBoxColumn coldinhluong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn coldonvi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colkcal;
+    }
+}
