07904a1fc39ee8e3ed5120486dfcd91520a882cf
xgiao <chipchipsieu@gmail.com>
2026-03-12 13:55:07 +0700

    Add files via upload

---
+++ frmnhanvien/frmnhanvien/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace frmnhanvien
+{
+    partial class frmnhanvien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýThựcPhẩmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhSáchThựcPhẩmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.địnhLượngTiêuChuẩnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thựcĐơnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.báoCáoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lậpThựcĐơnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngXuấtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tínhThựcPhẩmCầnSửDụngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.báoCáoThựcPhẩmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.báoCáoQuânSốToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hệThốngToolStripMenuItem,
+            this.quảnLýThựcPhẩmToolStripMenuItem,
+            this.thựcĐơnToolStripMenuItem,
+            this.báoCáoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hệThốngToolStripMenuItem
+            // 
+            this.hệThốngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.đăngXuấtToolStripMenuItem,
+            this.thoátToolStripMenuItem});
+            this.hệThốngToolStripMenuItem.Name = "hệThốngToolStripMenuItem";
+            this.hệThốngToolStripMenuItem.Size = new System.Drawing.Size(103, 29);
+            this.hệThốngToolStripMenuItem.Text = "Hệ thống";
+            this.hệThốngToolStripMenuItem.Click += new System.EventHandler(this.hệThốngToolStripMenuItem_Click);
+            // 
+            // quảnLýThựcPhẩmToolStripMenuItem
+            // 
+            this.quảnLýThựcPhẩmToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.danhSáchThựcPhẩmToolStripMenuItem,
+            this.địnhLượngTiêuChuẩnToolStripMenuItem});
+            this.quảnLýThựcPhẩmToolStripMenuItem.Name = "quảnLýThựcPhẩmToolStripMenuItem";
+            this.quảnLýThựcPhẩmToolStripMenuItem.Size = new System.Drawing.Size(180, 29);
+            this.quảnLýThựcPhẩmToolStripMenuItem.Text = "Quản lý thực phẩm";
+            // 
+            // danhSáchThựcPhẩmToolStripMenuItem
+            // 
+            this.danhSáchThựcPhẩmToolStripMenuItem.Name = "danhSáchThựcPhẩmToolStripMenuItem";
+            this.danhSáchThựcPhẩmToolStripMenuItem.Size = new System.Drawing.Size(289, 34);
+            this.danhSáchThựcPhẩmToolStripMenuItem.Text = "Danh sách thực phẩm";
+            this.danhSáchThựcPhẩmToolStripMenuItem.Click += new System.EventHandler(this.danhSáchThựcPhẩmToolStripMenuItem_Click);
+            // 
+            // địnhLượngTiêuChuẩnToolStripMenuItem
+            // 
+            this.địnhLượngTiêuChuẩnToolStripMenuItem.Name = "địnhLượngTiêuChuẩnToolStripMenuItem";
+            this.địnhLượngTiêuChuẩnToolStripMenuItem.Size = new System.Drawing.Size(289, 34);
+            this.địnhLượngTiêuChuẩnToolStripMenuItem.Text = "Định lượng tiêu chuẩn";
+            this.địnhLượngTiêuChuẩnToolStripMenuItem.Click += new System.EventHandler(this.địnhLượngTiêuChuẩnToolStripMenuItem_Click);
+            // 
+            // thựcĐơnToolStripMenuItem
+            // 
+            this.thựcĐơnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lậpThựcĐơnToolStripMenuItem});
+            this.thựcĐơnToolStripMenuItem.Name = "thựcĐơnToolStripMenuItem";
+            this.thựcĐơnToolStripMenuItem.Size = new System.Drawing.Size(103, 29);
+            this.thựcĐơnToolStripMenuItem.Text = "Thực đơn";
+            // 
+            // báoCáoToolStripMenuItem
+            // 
+            this.báoCáoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tínhThựcPhẩmCầnSửDụngToolStripMenuItem,
+            this.báoCáoThựcPhẩmToolStripMenuItem,
+            this.báoCáoQuânSốToolStripMenuItem});
+            this.báoCáoToolStripMenuItem.Name = "báoCáoToolStripMenuItem";
+            this.báoCáoToolStripMenuItem.Size = new System.Drawing.Size(91, 29);
+            this.báoCáoToolStripMenuItem.Text = "Báo cáo";
+            // 
+            // lậpThựcĐơnToolStripMenuItem
+            // 
+            this.lậpThựcĐơnToolStripMenuItem.Name = "lậpThựcĐơnToolStripMenuItem";
+            this.lậpThựcĐơnToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.lậpThựcĐơnToolStripMenuItem.Text = "Lập thực đơn";
+            this.lậpThựcĐơnToolStripMenuItem.Click += new System.EventHandler(this.lậpThựcĐơnToolStripMenuItem_Click);
+            // 
+            // đăngXuấtToolStripMenuItem
+            // 
+            this.đăngXuấtToolStripMenuItem.Name = "đăngXuấtToolStripMenuItem";
+            this.đăngXuấtToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.đăngXuấtToolStripMenuItem.Text = "Đăng xuất";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
+            // 
+            // tínhThựcPhẩmCầnSửDụngToolStripMenuItem
+            // 
+            this.tínhThựcPhẩmCầnSửDụngToolStripMenuItem.Name = "tínhThựcPhẩmCầnSửDụngToolStripMenuItem";
+            this.tínhThựcPhẩmCầnSửDụngToolStripMenuItem.Size = new System.Drawing.Size(341, 34);
+            this.tínhThựcPhẩmCầnSửDụngToolStripMenuItem.Text = "Tính thực phẩm cần sử dụng";
+            this.tínhThựcPhẩmCầnSửDụngToolStripMenuItem.Click += new System.EventHandler(this.tínhThựcPhẩmCầnSửDụngToolStripMenuItem_Click);
+            // 
+            // báoCáoThựcPhẩmToolStripMenuItem
+            // 
+            this.báoCáoThựcPhẩmToolStripMenuItem.Name = "báoCáoThựcPhẩmToolStripMenuItem";
+            this.báoCáoThựcPhẩmToolStripMenuItem.Size = new System.Drawing.Size(341, 34);
+            this.báoCáoThựcPhẩmToolStripMenuItem.Text = "Báo cáo thực phẩm";
+            this.báoCáoThựcPhẩmToolStripMenuItem.Click += new System.EventHandler(this.báoCáoThựcPhẩmToolStripMenuItem_Click);
+            // 
+            // báoCáoQuânSốToolStripMenuItem
+            // 
+            this.báoCáoQuânSốToolStripMenuItem.Name = "báoCáoQuânSốToolStripMenuItem";
+            this.báoCáoQuânSốToolStripMenuItem.Size = new System.Drawing.Size(341, 34);
+            this.báoCáoQuânSốToolStripMenuItem.Text = "Báo cáo quân số";
+            this.báoCáoQuânSốToolStripMenuItem.Click += new System.EventHandler(this.báoCáoQuânSốToolStripMenuItem_Click);
+            // 
+            // frmnhanvien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmnhanvien";
+            this.Text = "Nhân viên hậu cần";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hệThốngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýThựcPhẩmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem danhSáchThựcPhẩmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem địnhLượngTiêuChuẩnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thựcĐơnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem báoCáoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lậpThựcĐơnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem đăngXuấtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tínhThựcPhẩmCầnSửDụngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem báoCáoThựcPhẩmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem báoCáoQuânSốToolStripMenuItem;
+    }
+}
+
---
+++ frmnhanvien/frmnhanvien/frmbaocaothucpham.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace frmnhanvien
+{
+    partial class frmbaocaothucpham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblstart = new System.Windows.Forms.Label();
+            this.lblend = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.lblbuoi = new System.Windows.Forms.Label();
+            this.lblchedo = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.btnxuatbaocao = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colSTT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colstart = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colend = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colbuoi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colchedo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coltentp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colloai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colsoluong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coltongtien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnxuatbaocao);
+            this.splitContainer1.Panel1.Controls.Add(this.btnluu);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.lblchedo);
+            this.splitContainer1.Panel1.Controls.Add(this.lblbuoi);
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker2);
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker1);
+            this.splitContainer1.Panel1.Controls.Add(this.lblend);
+            this.splitContainer1.Panel1.Controls.Add(this.lblstart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(850, 450);
+            this.splitContainer1.SplitterDistance = 330;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lblstart
+            // 
+            this.lblstart.AutoSize = true;
+            this.lblstart.Location = new System.Drawing.Point(12, 56);
+            this.lblstart.Name = "lblstart";
+            this.lblstart.Size = new System.Drawing.Size(65, 18);
+            this.lblstart.TabIndex = 0;
+            this.lblstart.Text = "Từ ngày";
+            // 
+            // lblend
+            // 
+            this.lblend.AutoSize = true;
+            this.lblend.Location = new System.Drawing.Point(12, 134);
+            this.lblend.Name = "lblend";
+            this.lblend.Size = new System.Drawing.Size(74, 18);
+            this.lblend.TabIndex = 1;
+            this.lblend.Text = "Đến ngày";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(92, 56);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(92, 128);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // lblbuoi
+            // 
+            this.lblbuoi.AutoSize = true;
+            this.lblbuoi.Location = new System.Drawing.Point(12, 199);
+            this.lblbuoi.Name = "lblbuoi";
+            this.lblbuoi.Size = new System.Drawing.Size(40, 18);
+            this.lblbuoi.TabIndex = 4;
+            this.lblbuoi.Text = "Buổi";
+            // 
+            // lblchedo
+            // 
+            this.lblchedo.AutoSize = true;
+            this.lblchedo.Location = new System.Drawing.Point(12, 258);
+            this.lblchedo.Name = "lblchedo";
+            this.lblchedo.Size = new System.Drawing.Size(59, 18);
+            this.lblchedo.TabIndex = 5;
+            this.lblchedo.Text = "Chế độ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(92, 196);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(92, 255);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 26);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(53, 317);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(84, 34);
+            this.btnluu.TabIndex = 8;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = true;
+            // 
+            // btnxuatbaocao
+            // 
+            this.btnxuatbaocao.Location = new System.Drawing.Point(176, 316);
+            this.btnxuatbaocao.Name = "btnxuatbaocao";
+            this.btnxuatbaocao.Size = new System.Drawing.Size(84, 34);
+            this.btnxuatbaocao.TabIndex = 9;
+            this.btnxuatbaocao.Text = "Xuất";
+            this.btnxuatbaocao.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSTT,
+            this.colstart,
+            this.colend,
+            this.colbuoi,
+            this.colchedo,
+            this.coltentp,
+            this.colloai,
+            this.colsoluong,
+            this.coltongtien});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(516, 450);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // colSTT
+            // 
+            this.colSTT.HeaderText = "STT";
+            this.colSTT.MinimumWidth = 8;
+            this.colSTT.Name = "colSTT";
+            this.colSTT.Width = 70;
+            // 
+            // colstart
+            // 
+            this.colstart.HeaderText = "Từ ngày ";
+            this.colstart.MinimumWidth = 8;
+            this.colstart.Name = "colstart";
+            this.colstart.Width = 150;
+            // 
+            // colend
+            // 
+            this.colend.HeaderText = "Đến ngày";
+            this.colend.MinimumWidth = 8;
+            this.colend.Name = "colend";
+            this.colend.Width = 150;
+            // 
+            // colbuoi
+            // 
+            this.colbuoi.HeaderText = "Buổi";
+            this.colbuoi.MinimumWidth = 8;
+            this.colbuoi.Name = "colbuoi";
+            // 
+            // colchedo
+            // 
+            this.colchedo.HeaderText = "Chế độ";
+            this.colchedo.MinimumWidth = 8;
+            this.colchedo.Name = "colchedo";
+            // 
+            // coltentp
+            // 
+            this.coltentp.HeaderText = "Thực phẩm";
+            this.coltentp.MinimumWidth = 8;
+            this.coltentp.Name = "coltentp";
+            this.coltentp.Width = 150;
+            // 
+            // colloai
+            // 
+            this.colloai.HeaderText = "Loại";
+            this.colloai.MinimumWidth = 8;
+            this.colloai.Name = "colloai";
+            // 
+            // colsoluong
+            // 
+            this.colsoluong.HeaderText = "Số lượng";
+            this.colsoluong.MinimumWidth = 8;
+            this.colsoluong.Name = "colsoluong";
+            // 
+            // coltongtien
+            // 
+            this.coltongtien.HeaderText = "Tổng tiền";
+            this.coltongtien.MinimumWidth = 8;
+            this.coltongtien.Name = "coltongtien";
+            // 
+            // frmbaocaothucpham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmbaocaothucpham";
+            this.Text = "frmbaocaothucpham";
+            this.Load += new System.EventHandler(this.frmbaocaothucpham_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnxuatbaocao;
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblchedo;
+        private System.Windows.Forms.Label lblbuoi;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblend;
+        private System.Windows.Forms.Label lblstart;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSTT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colstart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colend;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colbuoi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colchedo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn coltentp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colloai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colsoluong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn coltongtien;
+    }
+}
