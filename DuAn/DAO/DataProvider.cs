aecf777cf781d2ab9ef0797c27b8db717d4741fa
Minh Hieu <minhhieu0147896@gmail.com>
2026-03-26 14:01:43 +0700

    dataprovider

---
+++ DuAn/DAO/DataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuAn.DAO
+{
+    internal class DataProvider
+    {
+        private string connectionString = "Data Source=localhost\\ SQLEXPRESS;Initial Catalog=Quan_li_tieu_chuan_QN;Integrated Security=True;Encrypt=False";
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
