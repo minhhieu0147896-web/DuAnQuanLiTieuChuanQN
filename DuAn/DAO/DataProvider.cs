e1460d5aa27b5ddb5516e326e927db45fb8c8fcd
Minh Hieu <minhhieu0147896@gmail.com>
2026-04-01 15:54:44 +0700

    them dataprovider

--- DuAn/DAO/DataProvider.cs
+++ DuAn/DAO/DataProvider.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DuAn.DAO
 {
@@ -25,6 +26,60 @@ namespace DuAn.DAO
                 connection.Close();
             }
             return data;
+        }
+
+        public int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            int rowsAffected = 0;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    if (parameters != null && parameters.Length > 0)
+                        cmd.Parameters.AddRange(parameters);
+
+                    rowsAffected = cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi thực thi: " + ex.Message, "Lỗi",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return rowsAffected;
+        }
+
+        public object ExecuteScalar(string query, params SqlParameter[] parameters)
+        {
+            object result = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    if (parameters != null && parameters.Length > 0)
+                        cmd.Parameters.AddRange(parameters);
+
+                    result = cmd.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi truy vấn: " + ex.Message, "Lỗi",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return result;
         }
     }
 }
