53eb988d9e402ecd9cb314d7d758e66ea9d1afab
xgiao <chipchipsieu@gmail.com>
2026-04-03 23:38:06 +0700

    login

--- DuAn/GUI/frmlogin/frmmhlogin.Designer.cs
+++ DuAn/GUI/frmlogin/frmmhlogin.Designer.cs
@@ -28,114 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmmhlogin));
+            this.pnlogin = new System.Windows.Forms.Panel();
+            this.lbldangnhap = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlogin.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // pnlogin
             // 
-            this.textBox1.Location = new System.Drawing.Point(143, 81);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(292, 26);
-            this.textBox1.TabIndex = 0;
+            this.pnlogin.BackColor = System.Drawing.Color.Gray;
+            this.pnlogin.Controls.Add(this.lbldangnhap);
+            this.pnlogin.Controls.Add(this.button2);
+            this.pnlogin.Controls.Add(this.button1);
+            this.pnlogin.Controls.Add(this.textBox3);
+            this.pnlogin.Controls.Add(this.label3);
+            this.pnlogin.Controls.Add(this.label2);
+            this.pnlogin.Controls.Add(this.textBox1);
+            this.pnlogin.Location = new System.Drawing.Point(243, 95);
+            this.pnlogin.Name = "pnlogin";
+            this.pnlogin.Size = new System.Drawing.Size(478, 400);
+            this.pnlogin.TabIndex = 6;
+            this.pnlogin.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // lbldangnhap
+            // 
+            this.lbldangnhap.AutoSize = true;
+            this.lbldangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldangnhap.ForeColor = System.Drawing.Color.Transparent;
+            this.lbldangnhap.Location = new System.Drawing.Point(162, 64);
+            this.lbldangnhap.Name = "lbldangnhap";
+            this.lbldangnhap.Size = new System.Drawing.Size(162, 32);
+            this.lbldangnhap.TabIndex = 10;
+            this.lbldangnhap.Text = "Đăng nhập";
+            this.lbldangnhap.Click += new System.EventHandler(this.lbldangnhap_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.button2.Location = new System.Drawing.Point(265, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(94, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Đăng nhập";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(143, 130);
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(150, 192);
             this.textBox3.Name = "textBox3";
             this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(292, 26);
-            this.textBox3.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(186, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 32);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Đăng nhập";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 84);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 18);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Tên đăng nhập";
+            this.textBox3.Size = new System.Drawing.Size(292, 19);
+            this.textBox3.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 130);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label3.Location = new System.Drawing.Point(16, 191);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 18);
-            this.label3.TabIndex = 5;
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 6;
             this.label3.Text = "Mật khẩu";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(143, 183);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Đăng nhập";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label2.Location = new System.Drawing.Point(3, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tên đăng nhập";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button2
+            // textBox1
             // 
-            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(307, 185);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 32);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Thoát";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(150, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 19);
+            this.textBox1.TabIndex = 1;
             // 
-            // frmlogin
+            // frmmhlogin
             // 
-            this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 308);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
-            this.Name = "frmlogin";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1022, 631);
+            this.Controls.Add(this.pnlogin);
+            this.DoubleBuffered = true;
+            this.Name = "frmmhlogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "login";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmlogin_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlogin.ResumeLayout(false);
+            this.pnlogin.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlogin;
+        private System.Windows.Forms.Label lbldangnhap;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- DuAn/GUI/frmlogin/frmmhlogin.cs
+++ DuAn/GUI/frmlogin/frmmhlogin.cs
@@ -21,7 +21,11 @@ namespace frmlogin
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            textBox1.BackColor = Color.Gainsboro;
+            textBox3.BackColor = Color.Gainsboro;
 
+            textBox1.ForeColor = Color.Black;
+            textBox3.ForeColor = Color.Black;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -78,6 +82,66 @@ namespace frmlogin
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            pnlogin.BackColor = Color.FromArgb(120, 0, 0, 0);
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            int radius = 30;
+
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(pnlogin.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(pnlogin.Width - radius, pnlogin.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, pnlogin.Height - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+
+            pnlogin.Region = new Region(path);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1.FlatAppearance.MouseOverBackColor = Color.DarkGreen;
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin đăng nhập", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                frm_manhinh_nhanvien frm = new frm_manhinh_nhanvien();
+                frm.Show();
+                this.Hide();
+
+                // connect form login vs form khac o day
+            }
+        }
+
+        private void lbldangnhap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát không?", "Xác nhận ",
+              MessageBoxButtons.YesNo,
+              MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
         }
     }
 }
