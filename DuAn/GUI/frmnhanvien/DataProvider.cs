f9548e76ab73089444db86a2e13b5a40a82e5cdf
Minh Hieu <minhhieu0147896@gmail.com>
2026-04-17 10:22:55 +0700

    s

---
+++ DuAn/GUI/frmnhanvien/DataProvider.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuAn.DAO
+{
+    internal class DataProvider
+    {
+        // Bước 1: Biến lưu instance duy nhất
+        private static DataProvider _instance;
+
+        // Bước 2: Property để truy cập instance từ bên ngoài
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new DataProvider();
+                return _instance;
+            }
+        }
+
+        // Bước 3: Constructor private - khóa không cho tạo bằng "new" từ bên ngoài
+        private DataProvider() { }
+
+        private string connectionString = "Data Source=localhost\\ SQLEXPRESS;Initial Catalog=Quan_li_tieu_chuan_QN;Integrated Security=True;Encrypt=False";
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            int rowsAffected = 0;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    if (parameters != null && parameters.Length > 0)
+                        cmd.Parameters.AddRange(parameters);
+
+                    rowsAffected = cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi thực thi: " + ex.Message, "Lỗi",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return rowsAffected;
+        }
+
+        public object ExecuteScalar(string query, params SqlParameter[] parameters)
+        {
+            object result = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    if (parameters != null && parameters.Length > 0)
+                        cmd.Parameters.AddRange(parameters);
+
+                    result = cmd.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi truy vấn: " + ex.Message, "Lỗi",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return result;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
